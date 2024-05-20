b2303e584fb82dba625ad7582bbe68dc8e9b4207
Ilyaplot99 <ilyaplot99@gmail.com>
2024-05-20 11:35:45 +0300

    Реализован подсчет пути

--- dfs.js
+++ dfs.js
@@ -1,11 +1,19 @@
 function dfs(graph, startNode) {
     let visited = [];
-    function dfsUtil(node) {
+    let pathLength = 0;
+
+    
+    function dfsUtil(node, count) {
         visited.push(node);
+    
+        if (node === endNode) {
+            pathLength = count;
+        }
+        
         let neighbors = graph[node];
         for (let i = 0; i < neighbors.length; i++) {
             if (!visited.includes(neighbors[i])) {
-                dfsUtil(neighbors[i]);
+                dfsUtil(neighbors[i], count + 1);
             }
         }
     }
@@ -20,6 +28,8 @@ function dfs(graph, startNode) {
     B: ['A', 'D', 'E'],
     C: ['A', 'F'],
     D: ['B'],
+
     }; 
 }
-    dfs(graph, 'A', 'F');
+    let pathLength = dfs(graph, 'A', 'F');
+    console.log("Length of path from A to F: " + pathLength);
